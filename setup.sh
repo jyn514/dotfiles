74e43b15245e452d17ed43bc6db3871edfffc244
jyn <github@jyn.dev>
2018-03-03 09:17:22 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -2,22 +2,23 @@
 set -e
 set -v
 
-# base config
-LOCAL="$HOME/.local/config"
-CONFIG=config
-for f in "$CONFIG"/*; do
-	if [ "$f" = "youtube-dl" ]; then
-		DEST="$HOME/.config/youtube-dl/config"
-	else
-		DEST="$HOME/.$f"
-	fi
-	if [ -L "$DEST" ]; then rm "$DEST"
-	elif [ -e "$DEST" ]; then
-			if ! [ -d "$LOCAL" ]; then mkdir "$LOCAL"; fi
-			mv "$DEST" "$LOCAL"
-	fi
-	ln -s "$CONFIG/$f" "$DEST"
-done
+setup_basics () {
+	LOCAL="$HOME/.local/config"
+	for f in "$(realpath config)"/*; do
+		if [ "$f" = "youtube-dl" ]; then
+			DEST="$HOME/.config/youtube-dl/config"
+		else
+			DEST="$HOME/.$(basename $f)"
+		fi
+		if [ -L "$DEST" ]; then rm -f "$DEST"
+		elif [ -e "$DEST" ]; then
+				if ! [ -d "$LOCAL" ]; then mkdir -p "$LOCAL"; fi
+				mv "$DEST" "$LOCAL"
+		fi
+		ln -s "$f" "$DEST"
+	done
+unset DEST LOCAL f
+}
 
 setup_shell () {
 	default_shell=$(grep "$USER" /etc/passwd | cut -d ':' -f 7)
@@ -26,27 +27,35 @@ setup_shell () {
 	elif ! grep bash "$default_shell"; then
 		if which bash; then chsh -s "$(which bash)"; fi
 	fi
-	unset default_shell
+unset default_shell
 }
 
-setup_shell
+setup_python () {
+	if [ -x "$(which pip)" ]; then
+		PYTHON="$(which pip)"
+	elif [ -x "$(which python)" ] && "$(which python)" -m pip > /dev/null; then
+		PYTHON="$(which python) -m pip"
+	fi
 
-if [ -x "$(which pip)" ]; then
-	PYTHON="$(which pip)"
-elif [ -x "$(which python)" ] && "$(which python)" -m pip > /dev/null; then
-	PYTHON="$(which python) -m pip"
-fi
+	# may take a while
+	if ! [ -z "$PYTHON" ]; then
+		$PYTHON install --user -r python.txt
+	fi
+unset PYTHON
+}
 
-# may take a while
-if ! [ -z "$PYTHON" ]; then
-	$PYTHON install --user -r "$CONFIG/../python.txt"
-fi
-
+setup_vim () {
 VIMDIR="$HOME/.vim/autoload"
-if ! [ -e "$VIMDIR/plug.vim" ]; then
-	curl -Lo "$VIMDIR/plug.vim" --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	vim -c PlugUpdate -c q -c q
-fi
+	if ! [ -e "$VIMDIR/plug.vim" ]; then
+		curl -Lo "$VIMDIR/plug.vim" --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+		vim -c PlugUpdate -c q -c q
+	fi
+unset VIMDIR
+}
 
-unset LOCAL CONFIG DEST VIMDIR
+cd "$(realpath "$(dirname "$0")")"
+setup_basics
+setup_shell
+setup_python
+setup_vim
