92f57966d37a07b94575ae62f3e2273183c9c190
Joshua Nelson <jyn514@gmail.com>
2019-10-03 14:20:14 -0400

    add compatibility with MacOS

--- setup.sh
+++ setup.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
-set -e
+set -eu
+
+startswith() {
+	case "$1" in
+		"$2"*) return 0;;
+		*) return 1;;
+	esac
+}
+
+realdir() {
+	cd "$1" && pwd -P
+}
+
+command -v realpath >/dev/null 2>&1 || realpath() {
+	if [ -d "$1" ]; then
+		realdir "$1"
+		return
+	fi
+	dir="$(realdir "$(dirname "$1")")"
+	if [ -L "$1" ]; then
+		file="$(readlink "$1")"
+	else
+		file="$1"
+	fi
+	# absolute path, ignore directory
+	if startswith "$file" /; then
+		echo "$file"
+		return
+	fi
+	echo "$dir/$file"
+}
 
 setup_basics () {
 	echo Installing configuration to ~
