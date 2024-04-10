df385cac2c335dfb370f727a464aafa12a1c2373
jyn <github@jyn.dev>
2024-04-10 15:51:26 -0400

    configure antigen locally
    this way it works even on alpine

--- lib/setup_sudo.sh
+++ lib/setup_sudo.sh
@@ -15,7 +15,7 @@ install_features () {
 		   python3-pip graphviz xdot xdg-utils \
 		   traceroute valgrind keepassxc rclone \
 		   curl jq tree pkg-config libssl-dev manpages manpages-dev bpytop git-absorb \
-		   ninja-build zsh-antigen
+		   ninja-build
 
 		# https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu
 		if ! exists pwsh; then
