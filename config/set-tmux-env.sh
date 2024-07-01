cd1eac5ae02e6765a3f74c2654c589829e1586b4
jyn <github@jyn.dev>
2024-06-30 20:14:53 -0400

    use bash instead of zsh in set-tmux-env for portability

--- config/set-tmux-env.sh
+++ config/set-tmux-env.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # tmux, annoyingly, does not read startup files :(
 # this matters when starting a new tmux session from ssh, where no environment variables are set by default
-env -i TERM="$TERM" PS1='; ' zsh --no-rcs --no-globalrcs -c '. ~/.profile && export PATH && export' | grep -E '^(EDITOR|VISUAL|PATH)=' | tr = '\n' | xargs -n2 tmux set-environment -g
+env -i HOME="$HOME" TERM="$TERM" PS1='; ' bash --noprofile --norc -c '. ~/.profile && export PATH && printenv' \
+	| grep -E '^(EDITOR|VISUAL|PATH)=' \
+	|  tr = '\n' | xargs -n2 tmux set-environment -g
