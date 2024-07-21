845839c3f0b93d31eeb2370049f3b7a2379469d5
jyn <github@jyn.dev>
2024-07-21 14:09:36 -0400

    oops

--- config/attach-session.sh
+++ config/attach-session.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# if we're coming from another session the empty session is intentional; don't override the explicit command.
+if [ "$(tmux display-message -p '#{client_last_session}' | tr -d '\n')" ]; then
+  exit 0
+fi
+
 # switch to the first detached session, if it exists
 target=$(tmux list-sessions -f '#{?session_attached,0,1}' -F '#{session_id}' | head -n1)
 if [ "$target" ]; then
