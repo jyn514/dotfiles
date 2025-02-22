3b7aea8c3761eae4f7f4f815b8112ec2fc438474
jyn <github@jyn.dev>
2025-02-22 16:47:22 -0500

    allow quotes in file names

--- lib/search-regex.sh
+++ lib/search-regex.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-start_delim='(^|/|\<|[[:space:]])'
+start_delim='(^|/|\<|[[:space:]"])'
+
 relative_path='(\.|\.\.)'
-start_path="($relative_path|[[:alnum:]~_-]*)"
-component='[][[:alnum:]_.#$%&+=@-]'
+start_path="($relative_path|[[:alnum:]~_\"-]*)"
+
+component='[][[:alnum:]_.#$%&+=@"-]'
 intermediate_paths="(/$component+)"
+
 line_no='(:[0-9]+)'
 file_end="($component+$line_no?$line_no?)"
-end="(/|\.$file_end|$component+$line_no$line_no?)"
+end="([/ \"]|\.$file_end|$component+$line_no$line_no?)"
+
 echo "$start_delim$start_path(${intermediate_paths}+$end|${intermediate_paths}{2,}$end?|$relative_path/$file_end)"
 
 # test cases; see https://regex101.com/r/Ct2v7A/1
