42fe3f37dda0d7731356c25e8ee851e872a9773b
jyn <github@jyn.dev>
2025-04-02 20:04:06 -0400

    fix attach-session interactions with tmux-cssh

--- config/attach-session.sh
+++ config/attach-session.sh
@@ -4,7 +4,7 @@
 case "$1" in
   disable) tmux set-option  -s  @attach-session-disable 1;;
   enable)  tmux set-option  -su @attach-session-disable;;
-  *)    if tmux show-option -sv @attach-session-disable>/dev/null; then exit 0; fi;;
+  *)    if tmux show-option -sv @attach-session-disable 2>/dev/null; then exit 0; fi;;
 esac
 
 # if we're coming from another session the empty session is intentional; don't override the explicit command.
@@ -12,10 +12,15 @@ if [ "$(tmux display-message -p '#{client_last_session}' | tr -d '\n')" ]; then
   exit 0
 fi
 
+# if *this* is a detached session, it was almost certainly created programmatically.
+# don't mess with scripts.
+if [ "$(tmux display-message -p '#{session_attached}' | tr -d '\n')" = 0 ]; then
+	exit 0
+fi
+
 # switch to the first detached session, if it exists
 target=$(tmux list-sessions -f '#{?session_attached,0,1}' -F '#{session_id}' | head -n1)
 if [ "$target" ]; then
-  # TODO: can we avoid spawning a shell just to immediately kill it?
   tmux set-option destroy-unattached
   tmux switch-client -t "$target"
 fi
