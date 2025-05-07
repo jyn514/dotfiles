a011b6355f2578ef503997a11afd59ce602a082b
jyn <github@jyn.dev>
2025-05-07 19:11:18 -0400

    fix docs

--- bin/ssh.sh
+++ bin/ssh.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # setup:
-# 1. `ln -s ~/work/vpn/config.conf /etc/openssh/client`
-# 2. disable `ProtectHome=true` in `/usr/lib/systemd/system/openvpn-client@.service`
-# 3. set windows terminal to `wsl.exe ~/work/ssh.sh dst tmux`
+# 1. `sudo ln -s ~/work/udp-vpn/config.conf /etc/openvpn/client/yottadb.conf`
+# 2. set `ProtectHome=read-only` in `/usr/lib/systemd/system/openvpn-client@.service`
+# 3. copy over /etc/hosts
+# 4. set windows terminal to `wsl.exe ~/work/ssh.sh dst tmux`
 s=openvpn-client@yottadb
 if ! systemctl is-active $s >/dev/null; then 
   sudo systemctl start $s
