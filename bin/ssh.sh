b6937ffcf1c0c436cf75679672c47ece9997643e
jyn <github@jyn.dev>
2025-05-07 19:45:26 -0400

    fix ssh script, again

--- bin/ssh.sh
+++ bin/ssh.sh
@@ -5,13 +5,16 @@
 # 3. copy over /etc/hosts
 # 4. set windows terminal to `wsl.exe ~/work/ssh.sh dst tmux`
 s=openvpn-client@yottadb
-if ! systemctl is-active $s >/dev/null; then 
-  sudo systemctl start $s
-  trap "sudo systemctl stop $s" EXIT
-fi
 if ! ssh-add -l >/dev/null 2>&1; then
 	eval $(ssh-agent)
 	ssh-add
 	ssh-add -l
+fi
+if ! systemctl is-active $s >/dev/null; then
+  sudo systemctl start $s
+  trap "sudo systemctl stop $s" EXIT
+	# hack: wait for dns server to startup
+	# i tried doing this the "proper" way with openvpn3 but it's horribly broken (it has a stateful "import" mechanism that doesn't work)
+	sleep 3
 fi
 ssh -At "$@"
