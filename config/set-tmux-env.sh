b4593788df0185c2fccf66f359730656eb6b705d
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2025-12-31 18:22:05 -0500

    fix tmux copy/paste on macOS

--- config/set-tmux-env.sh
+++ config/set-tmux-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # tmux, annoyingly, does not read startup files :(
 # this matters when starting a new tmux session from ssh, where no environment variables are set by default
-env -i HOME="$HOME" TERM="$TERM" PS1='; ' bash --noprofile --norc -c '. ~/.profile && export PATH && printenv' \
+env -i HOME="$HOME" TERM="$TERM" PS1='; ' bash --noprofile --norc -c '. /etc/profile && . ~/.profile && export PATH && printenv' \
 	| grep -E '^(EDITOR|VISUAL|PATH|CARGO_HOME|RUSTUP_HOME)=' \
 	|  tr = '\n' | xargs -n2 tmux set-environment
