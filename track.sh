170047beffd821d422e7ebf55f063618ea3d5d24
jyn <github@jyn.dev>
2026-01-02 23:39:19 -0500

    track.sh supports global files

--- track.sh
+++ track.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+dir=config
+if [ "$1" = --global ]; then
+	dir=global
+	shift
+fi
+
 if [ $# = 0 ] || [ $# -gt 2 ]; then
   echo "usage: $0 <existing file> [basename in config/]"
   exit 1
@@ -10,6 +16,12 @@ local=${2:-$(basename "$1")}
 cd "$(dirname "$0")"
 
 set -x
-mv "$1" "config/$local"
-ln -s "$(realpath "config/$local")" "$1"
-echo "$local=$(echo "$1" | sed "s#^$HOME/##")" >> config.txt
+echo "$local=$(echo "$1" | sed "s#^$HOME/##")" >> install/$dir.txt
+if [ $dir = global ]; then
+	# we have to be careful here, we don't want to break the system.
+	cp "$1" "$dir/$local"
+	sudo lib/setup_sudo.sh copy_globals --force
+else
+	mv "$1" "$dir/$local"
+	ln -s "$(realpath "$dir/$local")" "$1"
+fi
