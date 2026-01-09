88fcddf7e7bdab13cc0fcc48974c75c7cae63bea
jyn <github@jyn.dev>
2026-01-09 07:05:13 -0500

    fix kitty config finally lol lmao

--- config/glide.ts
+++ config/glide.ts
@@ -52,6 +52,7 @@ glide.autocmds.create("UrlEnter", {hostname: "app.fastmail.com"}, async () => {
 	glide.buf.keymaps.del("normal", "j");
 	glide.buf.keymaps.del("normal", "k");
 	glide.buf.keymaps.del("normal", "u");
+	glide.buf.keymaps.del("normal", "x");
 	glide.buf.keymaps.del("insert", "<C-i>");
 });
 glide.autocmds.create("UrlEnter", {hostname: "discord.com",}, async () => {
