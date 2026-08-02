ffa94c50411772a9e7b38fa6d27db8bd6344889c
Pi gpt-5.6-sol <325577925+one-esk-nineteen@users.noreply.github.com>
2026-08-02 16:30:30 +0200

    Harden shell completion initialization

--- config/z.fish
+++ config/z.fish
@@ -2,8 +2,8 @@ set -l curr_tok    (builtin commandline --current-process --current-token --cut-
 
 function __z_arguments
 	__fish_complete_directories "$curr_tok" ""
-	command zoxide query --exclude (__zoxide_pwd) -l -- $curr_tok \
-		| head -n20 | string replace $PWD/ "" | string replace $HOME "~"
+	set -l results (command zoxide query --exclude (__zoxide_pwd) -l -- $curr_tok); or return
+	printf '%s\n' $results | head -n20 | string replace $PWD/ "" | string replace $HOME "~"
 end
 
 complete -c __zoxide_z --no-files --keep-order -a '(__z_arguments)'
