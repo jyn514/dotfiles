5c3c36ae4528989a4521146bcb06fe2eb9f2b42a
Pi gpt-5.6-sol <breq@jyn.dev>
2026-08-21 20:07:07 +0000

    Add current date to Pi context

--- config/pi-extensions/index.ts
+++ config/pi-extensions/index.ts
@@ -1,9 +1,11 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import currentDate from "./current-date.ts";
 import instructionIncludes from "./pi-instruction-includes.ts";
 import notifyWhenSettled from "./pi-notify.ts";
 import webSearch from "./pi-web-search.ts";
 
 export default function dotfilesExtensions(pi: ExtensionAPI) {
+  currentDate(pi);
   notifyWhenSettled(pi);
   instructionIncludes(pi);
   webSearch(pi);
